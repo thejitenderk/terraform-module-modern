b8fc62f5a28ed22fbb77aa6429a17a4bf55125dd
Jitender Kumar <166748961+thejitenderk@users.noreply.github.com>
2025-12-05 23:17:21 +0530

    updated name (#2)
    * updated name
    
    * remove unused variable 'mssql_server_name' from sql_server_details

--- modules/azurerm_mssql_server/sql.tf
+++ modules/azurerm_mssql_server/sql.tf
@@ -1,4 +1,4 @@
-resource "azurerm_mssql_server" "example" {
+resource "azurerm_mssql_server" "mssql_server" {
   name                         = var.mssql_server_name
   resource_group_name          = var.resource_group_name
   location                     = var.location
