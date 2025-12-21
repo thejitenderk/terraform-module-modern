f349c73904789eed57627f77619ba7d25be37288
Jitender Kumar <166748961+thejitenderk@users.noreply.github.com>
2025-12-21 22:21:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,6 +6,7 @@ terraform {
     }
   }
  backend "azurerm" {
+  resource_group_name = "neha_rg"
   storage_account_name = "nehasg"
   container_name = "nehacontainer"
   key = "action.terraform.tfstate"
